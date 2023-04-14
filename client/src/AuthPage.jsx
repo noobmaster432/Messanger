74be082a923e08883c67dcdb8530a12c4ec01f34
Gyanendra Tiwari <mrgyan432@gmail.com>
2023-04-14 06:56:13 +0530

    changing backend url

--- client/src/AuthPage.jsx
+++ client/src/AuthPage.jsx
@@ -5,7 +5,9 @@ const AuthPage = (props) => {
     e.preventDefault();
     const { value } = e.target[0];
     axios
-      .post("http://localhost:3001/authenticate", { username: value })
+      .post("https://messanger-api.vercel.app/authenticate", {
+        username: value,
+      })
       .then((r) => props.onAuth({ ...r.data, secret: value }))
       .catch((e) => console.log("Auth Error", e));
   };
